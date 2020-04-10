9318bd5f3f9cca01c97e508327b4d375377917a4
Sky1Wu <wtx919463944@gmail.com>
2020-04-10 17:46:51 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Weibo_assistant
-获取微博关注列表的变动状态。
+# Weibo_Assistant
+
+获取微博关注列表的变动记录。
+
+## 功能
+
+通过 cookies 登录微博，首次运行将获取当前的关注列表并保存至本地，下次运行会再次获取最新关注列表与本地数据进行比对，将变动内容发送至 Telegram Group 或 Channel。
+
+## 配置
+
+1. 使用 Chrome 浏览器登录 [微博 H5](https://m.weibo.cn/)，按 `F12` 打开 `开发者工具`，选择 `Network - XHR`，刷新页面，在 `m.weibo.cn - Headers - Request Headers` 中复制 `cookie` 的内容，粘贴至脚本自定义区域中的 `cookies_str`。
+2. 在 `api_key` 中填写 Telegram Bot 的 `API Token`，可通过 [@BotFather](https://t.me/botfather) 获取。
+3. 在 `chat_id` 中填写 Telegram 目标 Group 或 Channel 的 `chat id`（将 Telegram Bot 添加至 Group 或 Channel，然后发送一条消息，即可通过 `https://api.telegram.org/bot<YourBOTToken>/getUpdates` 获取当前 `chat id`）。
+
+## 使用
+
+推荐使用 Python3 运行。
+
+需要安装以下内容
+
+```shell
+pip install python-telegram-bot requests
+```
+
+首次运行会在脚本同目录下创建 `weibo_follow_data.txt` 用于保存数据。
+
+可在服务器上配置 `crontab` 定时运行。
+
+example:
+
+```crontab
+0 */1 * * * python3 weibo_assistant.py # 每小时整点运行
+```
---
+++ weibo_assistant.py
@@ -0,0 +1,166 @@
+# coding: utf-8
+
+import requests
+import os
+import pickle
+import telegram
+import time
+from telegram import error as TGError
+
+# 自定义区域
+
+cookies_str = ''
+api_key = ''
+chat_id = ''
+
+# 自定义区域结束
+
+
+class DictDiffer(object):
+    def __init__(self, current_dict, past_dict):
+        self.current_dict, self.past_dict = current_dict, past_dict
+        self.set_current, self.set_past = set(
+            current_dict.keys()), set(past_dict.keys())
+        self.intersect = self.set_current.intersection(self.set_past)
+
+    def added(self):
+        return self.set_current - self.intersect
+
+    def removed(self):
+        return self.set_past - self.intersect
+
+    def changed(self):
+        return set(o for o in self.intersect if self.past_dict[o] != self.current_dict[o])
+
+
+def get_cookies(cookies_str):
+    cookies = {}
+    try:
+        for cookie in cookies_str.split(';'):
+            name, value = cookie.split('=')
+            cookies[name] = value
+        return cookies
+    except:
+        print('cookies 异常')
+        exit()
+
+
+def TimeStampToTime(timestamp):
+    timeStruct = time.localtime(timestamp)
+    return time.strftime('%Y-%m-%d %H:%M:%S', timeStruct)
+
+
+def get_FileModifyTime(filePath):
+    t = os.path.getmtime(filePath)
+    return TimeStampToTime(t)
+
+
+if __name__ == "__main__":
+    text = ''
+
+    cookies = get_cookies(cookies_str)
+
+    url = 'https://m.weibo.cn/api/container/getIndex'
+
+    page = 0
+    followed_count = 0
+
+    followed = {}
+
+    print('获取关注列表中...')
+
+    while True:
+        page += 1
+
+        data = {
+            'containerid': '231093_-_selffollowed',
+            'page': page
+        }
+
+        res = requests.get(url, params=data, cookies=cookies).json()
+
+        try:
+            if page == 1:
+                cards = res['data']['cards'][1]['card_group']
+            else:
+                cards = res['data']['cards'][0]['card_group']
+
+            for card in cards:
+                id = card['user']['id']
+                screen_name = card['user']['screen_name']
+                print(id, screen_name)
+                followed[id] = screen_name
+                followed_count += 1
+
+        except IndexError:
+            print('总关注：'+str(followed_count))
+            break
+        except KeyError:
+            print('获取关注列表失败，可能触发微博反爬虫或 cookies 失效')
+            try:
+                bot = telegram.Bot(token=api_key)
+                bot.sendMessage(chat_id=chat_id,
+                                text='微博小助手:\n获取关注列表失败，可能触发微博反爬虫或 cookies 失效')
+                exit()
+            except TGError.InvalidToken:
+                print('Telegram bot API token 非法')
+                exit()
+            except TGError.NetworkError:
+                print('Telegram 无法连接')
+                exit()
+
+    if os.path.exists('weibo_follow_data.txt'):
+        file = open('weibo_follow_data.txt', 'rb')
+        old = pickle.load(file)
+        file.close()
+
+        diff = DictDiffer(followed, old)
+
+        added = diff.added()
+        removed = diff.removed()
+        changed = diff.changed()
+
+        if added or removed or changed:
+            time_stamp = get_FileModifyTime('weibo_follow_data.txt')
+            text += ('距 '+time_stamp+'\n')
+            text += '关注列表发生如下变化\n\n'
+
+            if added:
+                print('新增以下关注：')
+                text += '新增以下关注：\n'
+                for _ in added:
+                    print(_, followed[_])
+                    text += (followed[_] + '\n')
+                text += '\n'
+
+            if removed:
+                print('减少以下关注：')
+                text += '减少以下关注：\n'
+                for _ in removed:
+                    print(_, old[_])
+                    text += (old[_] + '\n')
+                text += '\n'
+
+            if changed:
+                print('以下用户已修改 ID：')
+                text += '以下用户已修改 ID：\n'
+                for _ in changed:
+                    print('原 ID：'+old[_], '现 ID：'+followed[_])
+                    text += ('原 ID：'+old[_]+' '+'现 ID：'+followed[_]+'\n')
+                text += '\n'
+
+            text = ('微博小助手\n\n'+text+'\n')
+
+            try:
+                bot = telegram.Bot(token=api_key)
+                bot.sendMessage(chat_id=chat_id, text=text)
+            except TGError.InvalidToken:
+                print('Telegram bot API token 非法')
+                exit()
+            except TGError.NetworkError:
+                print('Telegram 无法连接')
+                exit()
+
+    file = open('weibo_follow_data.txt', 'wb')
+    pickle.dump(followed, file)
+    file.close()
